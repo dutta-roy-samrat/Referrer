70c424c652549037369f46531ad40004fc8c9a6d
Samrat Dutta Roy <samrat.roy@vinsol.com>
2025-03-04 23:20:46 +0530

    Remove environment-specific execution logic from build script

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -5,15 +5,3 @@ set -e  # Exit if any command fails
 poetry shell
 poetry install
 poetry run python manage.py migrate
-if [ "$ENVIRONMENT" = "production" ]; then
-    echo "Running in production mode"
-    exec daphne root.asgi:application -p 8000 -b 0.0.0.0
-
-elif [ "$ENVIRONMENT" = "development" ]; then
-    echo "Running in development mode"
-    exec poetry run python manage.py runserver 0.0.0.0:8000  
-
-else
-    echo "ERROR: ENVIRONMENT variable not set. Exiting."
-    exit 1
-fi
