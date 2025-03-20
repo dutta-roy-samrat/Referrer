b68ac24fa6a22498cccf13c34790dcb7cfe7b57c
Samrat Dutta Roy <samrat.roy@vinsol.com>
2025-03-20 19:58:02 +0530

    Enhance Dockerfile by adding dos2unix and updating server.sh to collect static files in production

--- backend/scripts/server.sh
+++ backend/scripts/server.sh
@@ -4,6 +4,7 @@ set -e  # Exit if any command fails
 
 poetry run python manage.py migrate
 if [ "$ENVIRONMENT" = "production" ]; then
+    poetry run python manage.py collectstatic --noinput
     echo "Running in production mode"
     exec daphne root.asgi:application -p 8000 -b 0.0.0.0
 
